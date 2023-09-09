dbcff65c78e233e8663648f8998f63ed188fe913
WagnerBenjamin <benjaminwagner@outlook.com>
2023-09-08 21:19:25 -0300

    modelo servidor

---
+++ controllers/demo.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+const { request, response} = require('express');
+const res = require('express/lib/response');
+
+
+module.exports = {
+   
+};
---
+++ models/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+
+class Server{
+    
+    constructor(){
+        this.port = process.env.PORT || 5000
+        this.app = express()
+        //primero en llamar
+        this.middleware()
+        //segundo en llamar
+        this.routers()
+        
+    }
+     
+    middleware(){
+        this.app.use(cors())
+        this.app.use(express.static('public'))
+    }
+
+    routers(){
+        this.app.get('/', (req, res) => {
+            res.send('Hello World!')
+          })
+        //v1 se pone por si actualizamos a una v2 pero la v1 sigue con vida (NO ES OBLIGATORIO)
+          this.app.use('/api/v1/demo', require('../routes/demo'))
+          
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Escuchando en el puerto ${this.port}`)
+          })
+    }
+}
+
+
+module.exports = Server
+
